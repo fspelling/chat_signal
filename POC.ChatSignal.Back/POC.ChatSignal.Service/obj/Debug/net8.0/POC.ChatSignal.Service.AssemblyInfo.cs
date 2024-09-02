37e9518883ede3a165bec9e499a54b94110f80e1
Fernando Spelling <fernando.spelling@etechss.com.br>
2024-09-02 12:08:38 -0300

    config git

--- POC.ChatSignal.Back/POC.ChatSignal.Service/obj/Debug/net8.0/POC.ChatSignal.Service.AssemblyInfo.cs
+++ POC.ChatSignal.Back/POC.ChatSignal.Service/obj/Debug/net8.0/POC.ChatSignal.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POC.ChatSignal.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af5d011537d1e0fbece5036f48f0aaf298be6cb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c18b323b9b4dd98bcb62a8f1f13dc348b711f2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("POC.ChatSignal.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POC.ChatSignal.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
